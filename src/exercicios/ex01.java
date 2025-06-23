43810b7e7dcb5009121472722a2510c71ef8c4a0
AlexRitter1 <ritter.alex@hotmail.com>
2025-06-23 20:20:51 -0300

    fix: (ex01) desafio resolvido com predicate e lambda

--- src/exercicios/ex01.java
+++ src/exercicios/ex01.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class ex01 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        numeros.stream().sorted().forEach(System.out::println);
+
+        numeros.stream().filter(n -> n % 2 == 0)
+                .sorted()
+                .forEach(System.out::println);
     }
 }
